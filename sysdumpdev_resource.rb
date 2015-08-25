96647486a797be0692ad2d152bdf49d7e138d207
Christophe Rousseau <christopherousseau@users.noreply.github.com>
2015-08-25 17:40:46 +0200

    Update sysdumpdev_resource.rb

--- sysdumpdev_resource.rb
+++ sysdumpdev_resource.rb
@@ -1,7 +1,8 @@
   actions :primary, :secondary, :estimate, :list, :dumpstats
   default_action :list
   attr_accessor :exists
-
+  
+  attribute :set_default, :kind_of => [TrueClass, FalseClass], :default => false
   attribute :device, :kind_of => String :name_attribute => true
   attribute :directory, :kind_of => [String, NilClass]
-  attribute :set_default, :kind_of => [TrueClass, FalseClass], :default => false
+
